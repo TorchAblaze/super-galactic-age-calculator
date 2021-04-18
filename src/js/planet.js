b0bd8565c382acf7cbdc66b0756cf1a83b6caea7
TorchAblaze <tiffanygreathead@gmail.com>
2021-04-17 22:08:02 -0700

    Add passing 'age is under 0' test for planetAge()

--- src/js/planet.js
+++ src/js/planet.js
@@ -4,6 +4,9 @@ export default class Planet {
     this.earthYears = earthYears;
   }
   planetAge(age) {
+    if (age < 0 || isNaN(age)) {
+      return false;
+    }
     const planetYears = parseInt(age / this.earthYears);
     this.planetYears = planetYears;
     return this.planetYears;
